67a5e5f4740cc3065abee0e084ea6441566e375d
DanishSharma1 <122099980+DanishSharma1@users.noreply.github.com>
2023-10-17 19:32:34 +0530

    Third Commit

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -128,7 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/images/'
 
-
+"""
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+"""
